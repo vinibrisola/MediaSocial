e1966c3c0d81aba5be42202b1882dc3d8cef41cb
Vinicius Brisola Alves <vinibrisola2@gmail.com>
2023-01-23 20:37:02 -0300

    config database

--- server/index.js
+++ server/index.js
@@ -33,3 +33,13 @@ const storage = multer.diskStorage({
   }
 })
 const upload = multer({ storage });
+
+
+/*MONGOOSE */
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParse: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  app.listen(PORT, () => console.log(`SERVER PORT: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`))
