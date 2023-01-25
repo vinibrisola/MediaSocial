884ca4385ec04cc119f53757c1a7d6c2a0d9f407
Vinicius Brisola Alves <vinibrisola2@gmail.com>
2023-01-25 06:49:19 -0300

    config dotenv

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ const upload = multer({ storage });
 
 
 /*ROUTES WITH FILES */
-app.post("/auth/register", upload.single("picture"), verifyToken, register);
+app.post("/auth/register", upload.single("picture"), register);
 
 /*ROUTES*/
 
