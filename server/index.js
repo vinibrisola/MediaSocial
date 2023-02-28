809c80d613760a4f09069a56b477089578b3fd55
Vinicius Brisola Alves <vinibrisola2@gmail.com>
2023-02-28 20:22:06 -0300

    backend data add and demo

--- server/index.js
+++ server/index.js
@@ -13,6 +13,9 @@ import userRoutes from './routes/users.js';
 import { register } from './controllers/auth.js';
 import { createPost } from './controllers/posts.js';
 import { verifyToken } from './middleware/auth.js';
+import User from './models/User.js';
+import Post from './models/Post.js';
+import { users, posts } from './data/index.js'
 
 /* CONFIG */
 const __filename = fileURLToPath(import.meta.url);
@@ -56,4 +59,6 @@ mongoose.connect(process.env.MONGO_URL, {
   useUnifiedTopology: true,
 }).then(() => {
   app.listen(PORT, () => console.log(`SERVER PORT: ${PORT}`));
+  /*User.insertMany(users);
+  Post.insertMany(posts);*/
 }).catch((error) => console.log(`${error} did not connect`));
